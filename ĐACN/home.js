14e59fb46f62099044395a265ad25175e7a2386b
diiafyra <dii.afyra.work@gmail.com>
2025-02-11 13:51:59 +0700

    hi

--- ĐACN/home.js
+++
@@ -1,7 +0,0 @@
-import { updateMenuVisibility } from './menuManager.js';
-document.addEventListener("DOMContentLoaded", function () {
-    let role = localStorage.getItem("role");
-    alert("role: " + role);
-    updateMenuVisibility(role);
-});
-
